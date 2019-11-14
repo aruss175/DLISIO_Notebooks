9dd95ebfd05757079d6c2e70dd575cbf19fc8343
Ashley Russell <43067386+aruss175@users.noreply.github.com>
2019-11-14 13:18:05 +0100

    Update test_curve_read.py

--- tests/test_curve_read.py
+++ tests/test_curve_read.py
@@ -1,8 +1,13 @@
 import dlisio
+import os
+import sys
+
 def test_curve_read():
-    curves_L = []
+    root = os.path.dirname( __file__ )
     file = 'data/COP_Pharos-1__VISION Service_CUSTOMER_482.11-5220.00mMD.dlis'
-    with dlisio.load(file) as logfile:
+    filepath = os.path.abspath(os.path.join(root, file))
+    curves_L = []
+    with dlisio.load(filepath) as logfile:
         for d in logfile:
             for channel in d.channels:
                 curves = channel.curves()
