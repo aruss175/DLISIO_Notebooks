85d435f84eb66bfcb926cd6ef6977e2fbe00fc97
Ashley Russell <43067386+aruss175@users.noreply.github.com>
2019-11-14 12:25:13 +0100

    Create test_header_origin.py

---
+++ tests/test_header_origin.py
@@ -0,0 +1,10 @@
+import dlisio
+
+def test_header_origin():
+    file = 'data/semantic.dlis'
+    with dlisio.load(file) as logfile:
+        for d in logfile:
+            for origin in d.origins:
+                originlength = len(str(origin))
+                print(originlength)
+    assert originlength > 0
