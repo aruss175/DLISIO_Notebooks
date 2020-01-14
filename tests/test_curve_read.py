4ed991ecea053f0ab47201f60a503f387698c35b
Ashley Russell <43067386+aruss175@users.noreply.github.com>
2020-01-14 11:03:13 +0100

    Update test_curve_read.py

--- tests/test_curve_read.py
+++ tests/test_curve_read.py
@@ -7,11 +7,10 @@ def test_curve_read():
     root = os.path.dirname( __file__ )
     file = 'data/COP_Pharos-1__VISION Service_CUSTOMER_482.11-5220.00mMD.dlis'
     filepath = os.path.abspath(os.path.join(root, file))
-    curves_L = []
     with dlisio.load(filepath) as logfile:
         for d in logfile:
-            for fram in d.frames:
-                for channel in fram.channels:
-                    curves = channel.curves()
-                    curves_L.append(curves)
-    assert len(curves_L) > 0
+            depth_channels = d.match('TDEP')
+            for channel in depth_channels:
+                depth_array = channel.curves()
+                print(len(depth_array))
+    assert len(depth_array) > 0
