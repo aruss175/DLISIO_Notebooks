eecec9250e9b8d2f09d34740686f1e899b81605b
dcslagel <dcs@mailworks.org>
2020-09-16 13:11:42 -0600

    Add test for new move_valid_index_to_first_col()

--- tests/test_curve_read.py
+++ tests/test_curve_read.py
@@ -3,6 +3,10 @@ dlisio.set_encodings(['latin1'])
 import os
 import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from func_dlis_to_las import move_valid_index_to_first_col
+
 def test_curve_read():
     root = os.path.dirname( __file__ )
     file = 'data/COP_Pharos-1__VISION Service_CUSTOMER_482.11-5220.00mMD.dlis'
@@ -14,3 +18,10 @@ def test_curve_read():
                 depth_array = channel.curves()
                 print(len(depth_array))
     assert len(depth_array) > 0
+
+
+def test_index_is_first_col():
+    curves_name = ['TDEP', 'DEPTH']
+    reordered_curves = move_valid_index_to_first_col(curves_name)
+    assert reordered_curves[0] == 'DEPTH'
+    assert reordered_curves[1] == 'TDEP'
